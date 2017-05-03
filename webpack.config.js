f872e039098c9a5bb067af981c4cfc5d77961b33
Tyler Kilburn <TylerKilburn@gmail.com>
2017-05-02 23:04:21 -0400

    Made the current nav item brighter.  Teacher will only show in teaching.hbs if a teacher exists.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+// require('file-loader');
 
-module.exports = {
-	entry: path.join(__dirname, 'src', 'public', 'js', 'nav-toggle.js'),
-	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'src', 'public', 'js'),
+module.exports = [
+	// Server Pack Config
+	// {
+	// 	entry: path.join(__dirname, 'src', 'public', 'js', 'nav-toggle.js'),
+	// 	output: {
+	// 		filename: 'bundle.js',
+	// 		path: path.join(__dirname, 'src', 'public', 'js'),
+	// 	},
+	// },
+	// Client Pack Config
+	{
+		context: path.join(__dirname, 'your-app'),
+		entry: path.join(__dirname, 'src', 'client', 'js', 'nav-toggle.js'),
+		output: {
+			filename: 'bundle.js',
+			path: path.join(__dirname, 'dist', 'client'),
+		},
+		plugins: [
+			new CopyWebpackPlugin([
+				{ from: path.join(__dirname, 'src', 'client', 'images') },
+			]),
+		],
 	},
-};
+];
